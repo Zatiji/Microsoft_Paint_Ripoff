cb491e3d5909ed58a11da8db53a165ae46cf5c5e
David M. <103978628+Zatiji@users.noreply.github.com>
2024-06-27 14:25:41 -0400

    adding the event listeners to buttons

--- Ms_Paint_code/src/SwingPaint.java
+++ Ms_Paint_code/src/SwingPaint.java
@@ -49,13 +49,39 @@ public class SwingPaint {
         
         // create controls to apply colors and call clear features
         JPanel controls = new JPanel();
-        
+
+
+        // Buttons
         clearBtn = new JButton("clear");
+        clearBtn.addActionListener(actionListener);
+
         blackBtn = new JButton("Black");
+        blackBtn.addActionListener(actionListener);
+
         blueBtn = new JButton("Blue");
+        blueBtn.addActionListener(actionListener);
+
         greenBtn = new JButton("green");
+        greenBtn.addActionListener(actionListener);
+
         redBtn = new JButton("Red");
+        redBtn.addActionListener(actionListener);
+
         magentaBtn = new JButton("Magenta");
+        magentaBtn.addActionListener(actionListener);
+
+        // add to panel
+        controls.add(greenBtn);
+        controls.add(blueBtn);
+        controls.add(blackBtn);
+        controls.add(redBtn);
+        controls.add(magentaBtn);
+        controls.add(clearBtn);
+
+        // add to content pane
+        content.add(controls, BorderLayout.NORTH);
+
+        frame.setSize(600, 600);
 
     }
 }
